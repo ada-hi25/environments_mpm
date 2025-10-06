4ca57a023771b0772c839f6e4be503f42827aae5
Harry <hi25@ic.ac.uk>
2025-10-06 19:55:10 +0100

    Pandas Script
    Added a new pandas script as a test. Updated dependencies and requirements.

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,9 +1,10 @@
 import numpy as np
+import pandas as pd
 from scipy.ndimage import gaussian_filter
 from scipy import misc
 
 
-__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve', 'my_pandas_series']
 
 
 def rand_array(shape):
@@ -14,3 +15,6 @@ def smooth_image(image, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def my_pandas_series(data):
+    return pd.Series(data)
---
+++ scripts/panda_series_creation.py
@@ -0,0 +1,4 @@
+import numpy as np
+from envtest import my_pandas_series
+
+print(my_pandas_series([1, 3, 5, np.nan, 6, 8]))
